f56183b0d01a934ff2436cb4d60cfd74b5fd5253
Yogesh-Oza <61262861+Yogesh-Oza@users.noreply.github.com>
2024-12-03 07:38:25 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import JobForm from './components/JobForm';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <JobForm />
+    </div>
+  );
+};
+
+export default App;
